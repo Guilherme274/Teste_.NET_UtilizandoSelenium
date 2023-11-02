410468e03cd3c09370d37979ffc7fd1ffa9787b6
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2023-11-02 16:55:28 -0300

    Adicionando Teste do EdgeWebDriver

---
+++ Alura.ByteBank.WebApp.Testes1/NavegandoNaPaginaHome.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.ByteBank.WebApp.Testes1
+{
+    public class NavegandoNaPaginaHome
+    {
+        [Fact]
+        public void CarregaPaginaHomeEVerificaTituloDaPagina()
+        {
+            //Arrange
+            IWebDriver driver = new EdgeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:44309");
+
+            //Assert
+            Assert.Contains("ByteBank-WebApp", driver.Title);
+        }
+    }
+}
